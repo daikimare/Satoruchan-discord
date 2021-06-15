2e95b64ff482bf4de904fa2cbe3a4c192225fe99
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-06-15 10:20:08 +0900

    first commit to main set up main file and src directory

---
+++ src/main.go
@@ -0,0 +1,13 @@
+package main
+
+// satoruchan ver:discord
+
+func main() {
+	// TODO: hooks discord room member(active)
+
+	// TODO: hooks discord room member(diconnect)
+
+	// TODO: check member name
+
+	// TODO: post name and message for slack room
+}
