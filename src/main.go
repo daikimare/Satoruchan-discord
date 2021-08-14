780d2ae689fb9f719d242abdfe174f183444d08a
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-08-14 18:24:47 +0900

    web server set up

--- src/main.go
+++ src/main.go
@@ -1,9 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	// _ "github.com/joho/godotenv/autoload"
+	// "github.com/bwmarrin/discordgo"
+)
+
 // satoruchan ver:discord
 
+var (
+	Token string
+)
+
 func main() {
+
+	router := gin.Default()
+	t := time.Now()
+	fmt.Println("Start App :",t)
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	env := os.Getenv("DISCORD_TOKEN")
+
+	router.GET("/", func (c *gin.Context)  {
+		c.JSON(200, gin.H{
+			"message": "satoruchan start",
+		})
+	})
+
+	fmt.Println(env)
+	router.Run(":3030")
+
 	// TODO: hooks discord room member(active)
+	router.POST("/checkMember", func (c *gin.Context)  {
+		n := c.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+
+		id.First()
+	})
 
 	// TODO: hooks discord room member(diconnect)
 
